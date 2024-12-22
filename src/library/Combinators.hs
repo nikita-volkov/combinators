e1b7c1c76941de340c7b3945413977497e5a97cc
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-12-22 13:37:21 +0300

    Add docs

--- src/library/Combinators.hs
+++ src/library/Combinators.hs
@@ -9,21 +9,37 @@ import Data.Function
 import Data.Int
 import Data.Traversable
 
--- | Generalization of 'listToMaybe', 'maybeToList', 'toList' and many others.
+-- * Alternation
+
+-- |
+-- Generalization over many common natural transformations, including:
+--
+-- - 'listToMaybe'
+-- - 'maybeToList'
+-- - 'toList'
+-- - @'either' ('const' 'Nothing') 'Just'@
 {-# INLINE alternate #-}
 alternate :: (Foldable f, Alternative g) => f a -> g a
 alternate = alternateMapM pure
 
+-- |
+-- 'alternate' extended with ability to map the wrapped value.
 {-# INLINE alternateMap #-}
 alternateMap :: (Foldable f, Alternative g) => (a -> b) -> f a -> g b
 alternateMap mapper = alternateMapM (pure . mapper)
 
+-- |
+-- 'alternateMap' extended with ability to do the mapping in the 'Alternative' context.
 {-# INLINE alternateMapM #-}
 alternateMapM :: (Foldable f, Alternative g) => (a -> g b) -> f a -> g b
 alternateMapM mapper = foldr cons empty
   where
     cons a b = mapper a <|> b
 
+-- * Traversal
+
+-- |
+-- Indexed version of 'forM'.
 {-# INLINE iforM #-}
 iforM :: (Monad m, Traversable f) => f a -> (Int -> a -> m b) -> m (f b)
 iforM collection f =
