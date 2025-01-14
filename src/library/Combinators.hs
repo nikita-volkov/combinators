19108eadd8753de225ae724ab87da1914e7dafdb
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-14 20:30:48 +0300

    Add intercalation

--- src/library/Combinators.hs
+++ src/library/Combinators.hs
@@ -3,10 +3,13 @@ module Combinators where
 import Control.Applicative
 import Control.Monad
 import Control.Monad.State.Strict
+import Data.Bool
 import Data.Foldable
 import Data.Function
 import Data.Int
+import Data.Monoid
 import Data.Traversable
+import Data.Tuple
 import GHC.Enum
 
 -- * Alternation
@@ -35,6 +38,30 @@ alternateMapM :: (Foldable f, Alternative g) => (a -> g b) -> f a -> g b
 alternateMapM mapper = foldr cons empty
   where
     cons a b = mapper a <|> b
+
+-- * Folding
+
+-- |
+-- A generic version of the original list-specialized version:
+--
+-- > intercalate :: [a] -> [[a]] -> [a]
+{-# INLINE intercalate #-}
+intercalate :: (Foldable f, Monoid a) => a -> f a -> a
+intercalate = flip intercalateMap id
+
+-- |
+-- 'intercalate' extended with ability to map the wrapped value.
+{-# INLINE intercalateMap #-}
+intercalateMap :: (Monoid m, Foldable f) => m -> (a -> m) -> f a -> m
+intercalateMap separator proj =
+  fst
+    . foldl'
+      ( \(acc, isFirst) element ->
+          if isFirst
+            then (proj element, False)
+            else (acc <> separator <> proj element, False)
+      )
+      (mempty, True)
 
 -- * Traversal
 
